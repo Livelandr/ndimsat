48bc23c50a44d080b8c2f7f12f301d5e5ad3ab5f
Livelandr <74109114+Livelandr@users.noreply.github.com>
2026-05-22 14:01:07 +0300

    Add copyright and license comments to main.cpp
    Added copyright notice and license information.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,8 @@
-﻿#include <iostream>
+// Copyright (C) 2026 Livelandr
+// This file is part of NDIM-SAT.
+// NDIM-SAT is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation...
+
+#include <iostream>
 #include <fstream>
 #include <vector>
 #include <cmath>
