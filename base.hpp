175658d4edf82d1f533320f3765bdca42a99df17
Livelandr <74109114+Livelandr@users.noreply.github.com>
2026-05-22 14:00:43 +0300

    Update base.hpp

--- base.hpp
+++ base.hpp
@@ -1,3 +1,7 @@
+// Copyright (C) 2026 Livelandr
+// This file is part of NDIM-SAT.
+// NDIM-SAT is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation...
+
 #pragma once
 
 namespace NDSAT {
